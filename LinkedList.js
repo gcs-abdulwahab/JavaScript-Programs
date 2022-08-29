28cb6adad50d18fba9bf8709141920f7520a8659
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2022-08-29 17:39:43 +0500

    Update LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -1,56 +1,98 @@
 class Node{
     constructor(data , link = null){
         this.data = data;
-        this.link = link;
+        this.link = link
     }
 };
 
 class LinkedList{
     constructor(){
         this.head = null;
+        this.last = null;
         this.length = 0;
     }
     
-    
-    isEmpty(){
-        return (this.length === 0);
-    }
+    isEmpty(){ return this.length == 0}
     
     insertAtHead(value){
-        var newNode = new Node(value);
-        newNode.link = this.head;
-        this.head = newNode;
+        var temp = new Node(value);
+        temp.link = this.head;
+        this.head = temp;
         this.length++;
-    }
-    
-    insertAtLast(value){
-        if (this.isEmpty){
-            this.head = new Node(value);
+
+        if (this.length == 1){
+            this.last = this.head;
         }
-        else {
-            var temp = this.head;
-            var newNode = new Node(value);
-            while (temp.link != null){
-                temp = temp.link;
-            }
-            temp.link = newNode;
-        }
-        this.length++;
     }
     
     print(){
         var temp = this.head;
-        while (temp != null){
-            process.stdout.write(temp.data + "->");
+        var str = "";
+        while(temp){
+            str += (temp.data + "->");
             temp = temp.link;
         }
-        console.log("NULL\n");
+        console.log(str+"NULL\n");
     }
+    
+    insertAtLast(value){
+        if (this.isEmpty()){
+            this.insertAtHead(value);
+            return
+        }
+        else {
+            var temp = this.head;
+            while (temp.link != null){
+                temp = temp.link
+            }
+            temp.link = new Node(value);
+            this.last = temp.link;
+            this.length++;
+        }
+    }
+        
+    removeAtHead(){
+        if (this.isEmpty()){
+            return;
+        }
+        else if (this.length == 1) {
+            this.head = null;
+            this.last = null;
+        }
+        else {
+            this.head = this.head.link;
+        }
+    }
+    
+    removeAtLast(){
+        if (this.isEmpty()){
+            return;
+        }
+        else if (this.length == 1){
+            this.head = null;
+            this.last = null;
+        }
+        else {
+            var temp = this.head;
+            while (temp.link.link != null){
+                temp = temp.link;
+            }
+            this.last = temp;
+            temp.link = null;
+        }
+    }
+        
 };
 
-var l1 = new LinkedList();
-l1.insertAtLast(4);
-l1.insertAtHead(3);
-l1.insertAtHead(2);
+l1 = new LinkedList();
+l1.insertAtLast(5);
 l1.insertAtHead(1);
-l1.print()
+l1.insertAtHead(2);
+l1.insertAtHead(3);
+l1.insertAtHead(4);
+l1.print();
+l1.removeAtHead();
+l1.removeAtLast();
+l1.print();
+console.log("head : " + l1.head.data);
+console.log("last : " + l1.last.data);
