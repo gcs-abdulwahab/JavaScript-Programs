c0954689f5d3d895f706c0426cc43a218d5e4ebc
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2022-08-29 18:24:47 +0500

    Update LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -101,7 +101,22 @@ class LinkedList{
         }
         return -1;
     }
-        
+    
+    reverse(){
+        if (!this.isEmpty() && this.length > 1){
+            var prev = null;
+            var next = null;
+            var curr = this.head;
+            this.last = this.head;
+            while (curr != null){
+                next = curr.link;
+                curr.link = prev;
+                prev = curr;
+                curr = next;
+            }
+            this.head = prev;
+        }
+    }    
 };
 
 l1 = new LinkedList();
@@ -111,9 +126,17 @@ l1.insertAtHead(2);
 l1.insertAtHead(3);
 l1.insertAtHead(4);
 l1.print();
-l1.removeAtHead();
-l1.removeAtLast();
+l1.reverse();
 l1.print();
 console.log("Length : " + l1.getLength());
 console.log("head : " + l1.getFirst());
 console.log("last : " + l1.getLast());
+
+// Output : 
+// 4->3->2->1->5->NULL
+
+// 5->1->2->3->4->NULL
+
+// Length : 5
+// head : 5
+// last : 4
