6e03ecb5d37e0189de01335f23d572a4a66ed0ae
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2022-08-28 23:58:47 -0800

    Create LinkedList.js

--- LinkedList.js
+++ LinkedList.js
@@ -24,17 +24,38 @@ class LinkedList{
             newNode.link = this.head;
             this.head = newNode;
         }
+        this.length++;
+    }
+    
+    insertAtLast(value){
+        if (this.isEmpty()){
+            head = new Node(value);
+        }
+        else {
+            var temp = this.head;
+            var newNode = new Node(value);
+            while (temp.link != null){
+                temp = temp.link;
+            }
+            temp.link = newNode;
+        }
+        this.length++;
     }
     
     print(){
         var temp = this.head;
         while (temp != null){
-            console.log(temp.data);
+            process.stdout.write(temp.data + "->");
             temp = temp.link;
         }
+        console.log("NULL\n");
     }
 };
 
 var l1 = new LinkedList();
 l1.insertAtHead(1);
+l1.insertAtHead(2);
+l1.insertAtHead(3);
 l1.print();
+
+
